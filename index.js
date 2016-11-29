cba0f64adb455f95ddf39d7ee7e7fe04d13246d7
thundergaz <wqiosa@163.com>
2016-11-29 17:09:26 +0800

    update gulp-tpl

--- index.js
+++ index.js
@@ -2,37 +2,13 @@
 
 var through = require('through2');
 var gutil = require('gulp-util');
-var ejs = require('ejs');
+var fs = require('fs');
 
 module.exports = function (options, settings) {
     settings = settings || {};
     options = options || {};
+    
     settings.ext = typeof settings.ext === "undefined" ? ".html" : settings.ext;
-
-    function formartStr(str){
-        var str=str.replace(/&gt;/g,'>');
-        var str=str.replace(/&lt;/g,'<');
-        var str=str.replace(/&#34;/g,'"');
-        return str;
-    }
-    function getoption(contents){
-        var option = contents.replace(/(?:<!--|\/\/-)\s*tpl:(\w+)\s*(\w+)\s*(?:-->)/g, function($1,$2,$3){
-                if(options[$2] == undefined) {
-                    options[$2] = $3;
-                }
-                return '';
-        });
-        var option = option.replace(/<block:(\w+)>([\s\S]*)<\/block:(\w+)>/g,function($1,$2,$3,$4){
-            console.log('$2');
-            console.log($2);
-
-            if (options[$2] == undefined ) {
-                options[$2] = $3;
-            }
-            return '';
-        })
-        return option;
-    }
     return through.obj(function (file, enc, cb) {
         if (file.isNull()) {
             this.push(file);
@@ -46,15 +22,77 @@ module.exports = function (options, settings) {
         }
         options.filename = file.path;
         try {
-            file.path = gutil.replaceExtension(file.path, settings.ext);           
+            file.path = gutil.replaceExtension(file.path, settings.ext);         
             var optionsn = getoption(file.contents.toString());
             file.contents = new Buffer(
-                formartStr(ejs.render(optionsn, file.data || options))
+                formartStr(changeStr(optionsn,options))
             );
         } catch (err) {
-            this.emit('error', new gutil.PluginError('gulp-ejs', err.toString()));
+            this.emit('error', new gutil.PluginError('gulp-tpl', err.toString()));
         }
         this.push(file);
         cb();
     });
+    function changeStr(content,option){
+         var content = content.replace(/<extend\sname="(\w+\/*\w+\.*\w+)"\s\/>/g,function($a,$b){
+                $b = $b.replace(/\//,"\\");
+               var tagname = option.filename.replace(/\w+.html$/,$b+'.html');
+                tagname = fs.readFileSync(tagname).toString();
+                return tagname;
+        })
+        return getplace(content,option);
+    }
+
+    function formartStr(str){
+        var str=str.replace(/&gt;/g,'>').replace(/&lt;/g,'<').replace(/&#34;/g,'"');
+        return str;
+    }
+
+    function getplace(content,option){
+        var str = /<block\sname="(\w+)">/g;
+        if(str.test(content)){
+         var content = content.replace(/<block\sname="(\w+)">([\w\W]*?)<\/block>/g,function($1,$2,$3){ 
+            if(str.test($3)){
+                return $1;
+            }
+            if(option[$2]){
+                return option[$2];
+            }
+            return $3;
+         })
+         if(str.test(content)){
+            var content = content.replace(/<block\sname="(\w+)">([\w\W]*)<\/block>/g,function($1,$2,$3){
+                if(options[$2]){
+                    return option[$2];
+                }
+                getplace($3,option);
+            })
+          }
+        }
+        return content;
+    }
+
+    function getoption(contents){
+        var str = /<block\sname="(\w+)">/g;
+            if(str.test(contents)){
+                var contents = contents.replace(/<block\sname="(\w+)">\r\n?([\w\W]*?)\r\n\t?<\/block>/g,function($1,$2,$3){
+                var str = /<block\sname="(\w+)">/g;
+                if(str.test($3)){
+                        return $1;
+                }else {
+                        options[$2]=$3;
+                        return '';
+                    }
+                })
+               if(str.test(contents)){
+                var contents = contents.replace(/<block\sname="(\w+)">\r\n\t?([\w\W]*)\r\n\t?<\/block>/g,function($1,$2,$3){
+                    options[$2]=$3.replace(/<block\sname="(\w+)">([\w\W]*?)<\/block>/g,'');
+                    getoption($3);
+                    return '';
+                })
+               } 
+            }
+            console.log(options);
+        return contents;
+    }
 };
